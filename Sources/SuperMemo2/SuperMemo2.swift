197a407eb6f033deea42623df7c22447a58ed413
Daichi Hayashi <daichikuwa0618@gmail.com>
2022-03-05 00:30:17 +0900

    add comment at SuperMemo2

--- Sources/SuperMemo2/SuperMemo2.swift
+++ Sources/SuperMemo2/SuperMemo2.swift
@@ -1,3 +1,11 @@
+//
+//  SuperMemo2.swift
+//
+//
+//  Created by Daichi Hayashi on 2022/02/27.
+//
+
+// c.f. https://www.supermemo.com/ja/archives1990-2015/english/ol/sm2
 public struct SuperMemo2 {
     public private(set) var text = "Hello, World!"
 
